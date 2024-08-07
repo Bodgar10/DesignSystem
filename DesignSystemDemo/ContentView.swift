8362ec8eeed1482e8c7f2c3dc9929e0003bf784d
Bodgar Espinosa <bodgar_jair@hotmail.com>
2024-08-07 13:31:24 -0600

    Update design system to open de demo for each component

--- DesignSystemDemo/ContentView.swift
+++ DesignSystemDemo/ContentView.swift
@@ -1,14 +1,35 @@
 import SwiftUI
+import DesignSystem
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Text("The DSM (Design System Manager) is a crucial tool for ensuring visual and interaction consistency in a mobile project. Its components are pre-built user interface elements")
+                Text("They are crafted for reuse across the application, guaranteeing uniform design and functionality on every screen.")
+                    .font(.system(size: 14))
+                    .foregroundColor(Color.gray)
+                NavigationLink(destination: ComponentListView()) { Text("01 - Components") }
+            }
+            .navigationBarTitle("DSMDemo")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+    }
+
+    // MARK: - Component List View
+
+    struct ComponentListView: View {
+        @State var sheetShowing: Bool = false
+        @State private var newViewShowing = false
+
+        var body: some View {
+            List {
+                NavigationLink(destination: Text("")) { Text("01 - TileViews") }
+                NavigationLink(destination: Text(""), label: { Text("02 - DominosAlertDemoView") })
+            }
+            .navigationBarTitle("Components")
+        }
     }
 }
 
